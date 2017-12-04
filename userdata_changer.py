d2242ed3e58db99bb552fe28eab90ba38a801c56
pkiel <intellhosp@gmail.com>
2017-12-04 13:42:53 +0100

    Update userdata_changer.

--- userdata_changer.py
+++ userdata_changer.py
@@ -4,44 +4,28 @@ import os
 allids = os.listdir("userdata/")
 newids = []
 
-
-last_line = '    "user_send_venue":1\n'
-
 input = '    "user_send_venue":1,\n    "user_attack_min":0,\n' + \
 '    "user_attack_max":15,\n    "user_defense_min":0,\n' + \
 '    "user_defense_max":15,\n    "user_stamina_min":0,\n' + \
-'    "user_stamina_max":15,\n' + '}\n'
+'    "user_stamina_max":15,\n' + '}'
 
 
 for x in allids:
     filename = "userdata/" + x
 
-
-
-    #filename = "data.json"
-
-
     f = open(filename, "r")
     lines = f.readlines()
     f.close()
 
-    f = open(filename, "w")
-    for line in lines:
-        if line!= "}"+"\n":
-            f.write(line)
-    f.close()
-
-    f = open(filename, "r")
-    lines = f.readlines()
-    f.close()
+    last_line = lines[:-1]
+    last_element = last_line[-1]
+    mod_last_element = ''.join((last_element[:-1],',\n'))
+    lines = lines[:-2]
+    lines.append(mod_last_element)
+    lines.append(input)
 
     f = open(filename, "w")
     for line in lines:
-        if line!= last_line:
-            f.write(line)
+        #if line!= "}":
+        f.write(line)
     f.close()
-
-
-    with open(filename, 'a') as file:
-        file.write(input)
-        file.close()
