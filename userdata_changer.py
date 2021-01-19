41d3658d2f2b3a5dfc0ddd8e44b4ee8d72dad2fb
Map Martin <map@vmd36533.contaboserver.net>
2021-01-19 19:25:44 +0100

    Upload static files. Update userdata_changer.py.

--- userdata_changer.py
+++ userdata_changer.py
@@ -5,17 +5,20 @@ allids = os.listdir("userdata/")
 newids = []
 
 #to_add = ["user_pvp_buddy","user_pvp_league_1500","user_pvp_league_2500"]
-to_add = ["user_pvp_max_rank"]
+#keys_to_add = ["user_pvp_max_level_40", "user_pvp_max_level_50"]
+#values_to_add = [False, True]
+
 
 for x in allids:
     filename = "userdata/" + x
 
     with open(filename, 'r', encoding='utf-8') as f:
         user_settings = json.loads(f.read())
-    #print(user_settings)
-    for i in to_add:
-        user_settings[i] = 100 #False
-    #print(user_settings)
+    #for i in to_add:
+        #user_settings[i] = False
+    user_settings["user_pvp_max_level_40"] = False
+    user_settings["user_pvp_max_level_50"] = True
+
     with open(filename, 'w') as fp:
         json.dump(user_settings, fp)
 
